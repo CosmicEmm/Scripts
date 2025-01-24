ea7230195c4b92e071544c6f4c53438d9bb19ff3
Muhammad <muhammadmalik5303@gmail.com>
2025-01-25 02:45:14 +0500

    Updated

--- player_wiki.py
+++ player_wiki.py
@@ -3,12 +3,12 @@ import lxml # not used directly, but pandas uses lxml internally for parsing
 from sys import argv
 
 
-scraper = pd.read_html(f'https://en.wikipedia.org/wiki/{argv[1]}')
+scraper = pd.read_html(f'https://en.wikipedia.org/wiki/{argv[1]}') #use _ instead of space in player name
 print(argv)
 
-for index, table in enumerate(scraper): # Iterates over the DataFrames
-    print('********************************************')
-    print(index)
-    print(table)
+# for index, table in enumerate(scraper): # Iterates over the DataFrames
+#     print('********************************************')
+#     print(index)
+#     print(table)
 
 print(scraper[2]) # Prints the 3rd table (index 2) from the list of DataFrames
