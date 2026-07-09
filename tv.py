2b4ecada27eddc5f18b2f3a03f7d1901a18ee997
CosmicEmm <muhammadmalik5303@gmail.com>
2026-07-09 16:35:47 +0500

    Updated

--- tv.py
+++ tv.py
@@ -7,9 +7,25 @@ CHANNELS = {
         "hls",
         "https://live.alarabiya.net/alarabiapublish/alarabiya.smil/playlist.m3u8",
     ),
+    "ekhbariya": (
+        "hls",
+        "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-al-ekhbaria/297b3ef1cd0633ad9cfba7473a686a06/index.m3u8",
+    ),
     "alqahera": (
         "yt",
         "https://www.youtube.com/live/E3e0iVSFgPI?si=E9ASzSuHp0ODwQd5",
+    ),
+    "iraqia": (
+        "hls",
+        "https://imn-live.esite-lab.com/hls/iraqia-news.m3u8?sid=rdmcy5e6mok2j3ab",
+    ),
+    "roya": (
+        "yt",
+        "https://www.youtube.com/@RoyaNews/live",
+    ),
+    "cbs": (
+        "hls",
+        "https://news20e7hhcb.airspace-cdn.cbsivideo.com/index.m3u8",
     ),
 }
 
